a5e35cb790fbcd44eacb47e4adc78293611a565d
Hardik <hardikkhandelwal500@gmail.com>
2021-03-26 00:10:38 +0530

    [add] logic to download folders + files in frontend

--- proof-of-concepts/test-frontend/src/Files2.js
+++ proof-of-concepts/test-frontend/src/Files2.js
@@ -1,115 +1,128 @@
-import './index.css'
-import axios from 'axios';
-import Dropzone from 'react-dropzone';
-import { useEffect, useState } from 'react';
+import "./index.css";
+import axios from "axios";
+import Dropzone from "react-dropzone";
+import { useEffect, useState } from "react";
+import { saveAs } from "file-saver";
 
 export default function FilesComponent() {
-    const [source, setSource] = useState(null)
-    const [folderID, setFolderID] = useState('605256109934f80db98712ea');
+    const [source, setSource] = useState(null);
+    const [folderID, setFolderID] = useState("605256109934f80db98712ea");
     // const [pathHashMap, setPathHashMap] = useState(null)
-    const ownerID = '605256109934f80db98712ea';
+    const ownerID = "605256109934f80db98712ea";
     const [files, setFiles] = useState([]);
 
     useEffect(() => {
         const CancelToken = axios.CancelToken;
         setSource(CancelToken.source());
-    }, [])
+    }, []);
 
     useEffect(() => {
         async function fetchData() {
-            const result = await axios.post(`http://localhost:3001/folders`, { ownerID, parentID: folderID })
-            setFiles(result.data)
+            const result = await axios.post(`http://localhost:3001/folders`, {
+                ownerID,
+                parentID: folderID,
+            });
+            setFiles(result.data);
         }
 
         fetchData();
-    }, [folderID])
+    }, [folderID]);
 
     const changeFolder = (newID) => {
-        setFolderID(newID)
-    }
+        setFolderID(newID);
+    };
 
     async function postFolders(folders) {
-        let directoryStructure = {}
-        let formData = new FormData()
-        formData.append(folders, JSON.stringify(folders))
+        let directoryStructure = {};
+        let formData = new FormData();
+        formData.append(folders, JSON.stringify(folders));
 
         try {
-            const { data } = await axios.post("http://localhost:3001/create_directory", formData)
+            const { data } = await axios.post(
+                "http://localhost:3001/create_directory",
+                formData
+            );
             directoryStructure = data.result;
 
-            await new Promise(resolve => setTimeout(resolve, 50));
+            await new Promise((resolve) => setTimeout(resolve, 50));
         } catch (err) {
             if (axios.isCancel(err)) {
                 console.log(err.message);
             }
-            console.log(err.message)
+            console.log(err.message);
         }
 
         for (let i = 0; i < folders.length; i++) {
-            let file_path_arr = folders[i].path.split("/")
-            file_path_arr.pop()
-            let file_path = file_path_arr.join("/")
-            const parent_id = directoryStructure[file_path]
+            let file_path_arr = folders[i].path.split("/");
+            file_path_arr.pop();
+            let file_path = file_path_arr.join("/");
+            const parent_id = directoryStructure[file_path];
             const parent = parent_id;
 
-            formData.append(parent, folders[i])
+            formData.append(parent, folders[i]);
         }
 
         const options = {
             cancelToken: source.token,
             onUploadProgress: (progressEvent) => {
                 const { loaded, total } = progressEvent;
-                let percent = Math.floor(loaded * 100 / total);
-                // console.log(`${loaded}kb of ${total} | ${percent}%`);
-            }
-        }
+                let percent = Math.floor((loaded * 100) / total);
+                console.log(`${loaded}kb of ${total} | ${percent}%`);
+            },
+        };
 
         try {
-            const { data } = await axios.post("http://localhost:3001/upload", formData, options)
+            const { data } = await axios.post(
+                "http://localhost:3001/upload",
+                formData,
+                options
+            );
 
             console.log(data);
 
-            await new Promise(resolve => setTimeout(resolve, 50));
+            await new Promise((resolve) => setTimeout(resolve, 50));
         } catch (err) {
             if (axios.isCancel(err)) {
                 console.log(err.message);
             }
-            console.log(err.message)
+            console.log(err.message);
         }
-
     }
 
     async function postFiles(files) {
-
-        let formData = new FormData()
+        let formData = new FormData();
 
         for (let i = 0; i < files.length; i++) {
-            const parent_id = folderID
+            const parent_id = folderID;
             const parent = parent_id;
 
-            formData.append(parent, files[i])
+            formData.append(parent, files[i]);
         }
 
         const options = {
             cancelToken: source.token,
             onUploadProgress: (progressEvent) => {
                 const { loaded, total } = progressEvent;
-                let percent = Math.floor(loaded * 100 / total);
-                // console.log(`${loaded}kb of ${total} | ${percent}%`);
-            }
-        }
+                let percent = Math.floor((loaded * 100) / total);
+                console.log(`${loaded}kb of ${total} | ${percent}%`);
+            },
+        };
 
         try {
-            const { data } = await axios.post("http://localhost:3001/upload", formData, options)
+            const { data } = await axios.post(
+                "http://localhost:3001/upload",
+                formData,
+                options
+            );
 
             console.log(data);
 
-            await new Promise(resolve => setTimeout(resolve, 50));
+            await new Promise((resolve) => setTimeout(resolve, 50));
         } catch (err) {
             if (axios.isCancel(err)) {
                 console.log(err.message);
             }
-            console.log(err.message)
+            console.log(err.message);
         }
     }
 
@@ -118,38 +131,61 @@ export default function FilesComponent() {
 
         console.log(files);
 
-        const listOfFolders = files.filter((e) => e.path !== e.name)
-        const listOfFiles = files.filter((e) => e.path === e.name)
+        const listOfFolders = files.filter((e) => e.path !== e.name);
+        const listOfFiles = files.filter((e) => e.path === e.name);
 
         if (listOfFolders.length) {
             console.log("FOLDERS FOUND : ", listOfFolders.length);
-            await postFolders(listOfFolders)
+            await postFolders(listOfFolders);
             console.log("FOLDERS SENT");
         }
 
         if (listOfFiles.length) {
             console.log("FILES FOUND : ", listOfFiles.length);
-            await postFiles(listOfFiles)
+            await postFiles(listOfFiles);
             console.log("FILES SENT");
         }
     }
 
+    async function downloadFolder() {
+        axios
+            .get("http://localhost:3001/download_directory", {
+                responseType: "blob",
+            })
+            .then((response) => new Blob([response.data]))
+            .then((blob) => saveAs(blob, "file.zip"));
+    }
+
+    async function downloadFile() {
+        axios
+            .get("http://localhost:3001/download_file", { responseType: "blob" })
+            .then(
+                (response) => new Blob([response.data], { type: "application/png" })
+            )
+            .then((blob) => saveAs(blob, "file1.png"));
+    }
+
     return (
         <div>
-            {
-                files.length ? (
-                    files.map(file => {
-                        return (
-                            <div key={file._id}>
-                                <div>{file.name}</div>
-                                {file.directory && <button onClick={() => changeFolder(file._id)}>OPEN FOLDER</button>}
-                            </div>
-                        )
-                    })
-                ) : null
-            }
+            {files.length
+                ? files.map((file) => {
+                    return (
+                        <div key={file._id}>
+                            <div>{file.name}</div>
+                            {file.directory && (
+                                <button onClick={() => changeFolder(file._id)}>
+                                    OPEN FOLDER
+                                </button>
+                            )}
+                        </div>
+                    );
+                })
+                : null}
 
-            <Dropzone onDrop={acceptedFiles => sendTheFiles(acceptedFiles)}>
+            <button onClick={() => downloadFolder()}>Download Folder</button>
+            <button onClick={() => downloadFile()}>Download File</button>
+
+            <Dropzone onDrop={(acceptedFiles) => sendTheFiles(acceptedFiles)}>
                 {({ getRootProps, getInputProps }) => (
                     <section>
                         <div {...getRootProps()} className="drop-zone">
@@ -160,5 +196,5 @@ export default function FilesComponent() {
                 )}
             </Dropzone>
         </div>
-    )
+    );
 }
