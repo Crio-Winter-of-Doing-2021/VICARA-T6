b9f371a46a5805e747e011bd07c04b3fd2a059a1
Hardik <hardikkhandelwal500@gmail.com>
2021-04-02 23:02:45 +0530

    [add] pre signed urls

--- backend/routes/download_file/index.ts
+++ backend/routes/download_file/index.ts
@@ -14,13 +14,22 @@ downloadFile.get("/", async (req, res, next) => {
   };
 
   try {
-    s3.getObject(params, function (err, data) {
-      if (err === null) {
-        res.send(data.Body);
-      } else {
-        res.status(500).send(err);
-      }
+    // s3.getObject(params, function (err, data) {
+    //   if (err === null) {
+    //     res.send(data.Body);
+    //   } else {
+    //     res.status(500).send(err);
+    //   }
+    // });
+
+    const url = s3.getSignedUrl("getObject", {
+      Bucket: process.env.S3_BUCKET_NAME,
+      Key: "606639090da8d65590e754f5",
+      Expires: 60,
+      ResponseContentDisposition: 'attachment; filename ="' + "video.mkv" + '"',
     });
+
+    res.status(200).send(url);
   } catch (error) {
     console.log(error);
   }
