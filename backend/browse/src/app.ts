ab07162dae8e9628dfbae7a9eaa50a27a1ac169e
Shubham Anand <shubham.rubal31@gmail.com>
2021-04-05 22:10:03 +0530

    Set cors headers manually in microservices

--- backend/browse/src/app.ts
+++ backend/browse/src/app.ts
@@ -19,10 +19,19 @@ app.use(cookieSession({
     secure: process.env.NODE_ENV === 'production',
     sameSite: 'none'
 }));
-app.use(cors({
-    origin: true,
-    credentials: true
-}));
+// app.use(cors({
+//     credentials: true
+// }));
+app.use((req, res, next) => {
+    // const allowedOrigins = ['http://127.0.0.1:8020', 'http://localhost:8020', 'http://127.0.0.1:9000', 'http://localhost:9000'];
+    const origin = req.headers.origin!;
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+    // res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    // res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    return next();
+});
 
 app.use(currentUser);
 app.use(requireAuth);
