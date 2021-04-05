ffb2fd9593593ecb820be43caf48dfffcb87123d
Hardik Khandelwal <hardikkhandelwal500@gmail.com>
2021-04-06 00:17:47 +0530

    [add] storage and starFiles routes

---
+++ backend/browse/src/routes/getAvailableStorage.ts
@@ -0,0 +1,41 @@
+import express, { Request, Response } from "express";
+import { Types } from "mongoose";
+import { checkIdValid, checkUpdateIdParam } from "../util/checkParams";
+import { File } from "../models/file.model";
+
+const router = express.Router();
+
+router.get("/api/star", async (req: Request, res: Response) => {
+  const ownerId = req.currentUser!.id;
+  const totalAllotedSize = 1024 * 1024 * 1024;
+
+  const totalUsedSize = await File.aggregate([
+    {
+      $match: {
+        $and: [
+          {
+            owner: new Types.ObjectId(ownerId),
+          },
+          {
+            directory: false,
+          },
+        ],
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        total: {
+          $sum: "$size",
+        },
+      },
+    },
+  ]);
+
+  return res.send({
+    totalAllotedSize: totalAllotedSize,
+    totalUsedSize: totalUsedSize?.[0]?.total ?? 0,
+  });
+});
+
+export { router as fileUpdateRouter };
---
+++ backend/browse/src/routes/starFiles.ts
@@ -0,0 +1,37 @@
+import express, { Request, Response } from "express";
+
+import { checkIdValid, checkUpdateIdParam } from "../util/checkParams";
+import { File } from "../models/file.model";
+import { BadUploadRequestError } from "@vic-common/common";
+
+const router = express.Router();
+
+router.get("/api/star", async (req: Request, res: Response) => {
+  const ownerId = req.currentUser!.id;
+
+  const starredFiles = await File.find({ owner: ownerId, starred: true });
+
+  return res.send(starredFiles);
+});
+
+router.patch("/api/star/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const ownerId = req.currentUser!.id;
+
+  let err = await checkUpdateIdParam(id, ownerId);
+  if (err.length !== 0) {
+    throw new BadUploadRequestError(err);
+  }
+
+  const fileDetails = await File.findById(id);
+
+  if (fileDetails?.isStarred === true) {
+    const updatedFile = await File.findByIdAndUpdate(id, { starred: false });
+    res.send(updatedFile);
+  } else {
+    const updatedFile = await File.findByIdAndUpdate(id, { starred: true });
+    res.send(updatedFile);
+  }
+});
+
+export { router as fileUpdateRouter };
