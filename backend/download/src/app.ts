fa1fa18682a9dffe8a06410048494138e532c71b
Shubham Anand <shubham.rubal31@gmail.com>
2021-04-05 23:17:05 +0530

    Clone auth app.ts to other microservices

--- backend/download/src/app.ts
+++ backend/download/src/app.ts
@@ -3,9 +3,9 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cors from 'cors';
 import cookieSession from 'cookie-session';
+import {errorHandler, NotFoundError} from '@vic-common/common';
 
-import {errorHandler, NotFoundError, currentUser, requireAuth} from '@vic-common/common';
-import { fileDownloadRouter } from './routes/downloadFile';
+import {fileDownloadRouter} from "./routes/downloadFile";
 
 const app = express();
 app.set('trust proxy', true);
@@ -15,23 +15,11 @@ app.use(cookieSession({
     secure: process.env.NODE_ENV === 'production',
     sameSite: 'none'
 }));
-// app.use(cors({
-//     origin: true,
-//     credentials: true
-// }));
-app.use((req, res, next) => {
-    // const allowedOrigins = ['http://127.0.0.1:8020', 'http://localhost:8020', 'http://127.0.0.1:9000', 'http://localhost:9000'];
-    const origin = req.headers.origin!;
-    res.setHeader('Access-Control-Allow-Origin', origin);
-    //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
-    // res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    // res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    return next();
-});
-
-app.use(currentUser);
-app.use(requireAuth);
+// const whitelist = ['http://localhost:3000', 'https://vigorous-dijkstra-746efd.netlify.app'];
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
 
 app.use(fileDownloadRouter);
 
