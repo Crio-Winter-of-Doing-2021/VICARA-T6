ae7a2e20781e26efb1610a15a54cb95ba71d42e1
Shubham Anand <shubham.rubal31@gmail.com>
2021-04-09 02:42:07 +0530

    Added check for size if no files exist

--- backend/upload/src/util/getStorage.ts
+++ backend/upload/src/util/getStorage.ts
@@ -2,6 +2,16 @@ import {File} from "../models/file.model";
 
 export const getAvailableStorage = async (ownerId: string) => {
     const TOTAL_ALLOTTED_SIZE = 1024 * 1024 * 100;
+
+    const fileExists = await File.findOne({
+        ownerId,
+        isDirectory: false
+    });
+
+    if (!fileExists) {
+        return TOTAL_ALLOTTED_SIZE;
+    }
+
     const totalUsedSize = await File.aggregate([
         {
             $match: {
@@ -24,5 +34,9 @@ export const getAvailableStorage = async (ownerId: string) => {
             },
         },
     ]);
+
+    console.log('TOTAL_USED_SIZE_FN');
+    console.log({totalUsedSize});
+    console.log(TOTAL_ALLOTTED_SIZE - totalUsedSize?.[0]?.total ?? 0);
     return TOTAL_ALLOTTED_SIZE - totalUsedSize?.[0]?.total ?? 0;
 }
